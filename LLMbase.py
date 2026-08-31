3f5429c61c9ec5869110c44b26f5c56b5177d683
Aman Nindra <amannindra@gmail.com>
2026-08-31 14:41:05 -0700

    dsa

--- LLMbase.py
+++ LLMbase.py
@@ -1,7 +1,6 @@
 from transformers import AutoProcessor, AutoModelForMultimodalLM
-
 MODEL_ID = "google/gemma-4-12B-it"
-
+from time import perf_counter
 # Load model
 processor = AutoProcessor.from_pretrained(MODEL_ID)
 model = AutoModelForMultimodalLM.from_pretrained(
@@ -27,9 +26,15 @@ inputs = processor.apply_chat_template(
 ).to(model.device)
 input_len = inputs["input_ids"].shape[-1]
 
+
+perf = perf_counter()
+print(f"Model now Generates: {perf}")
 # Generate output
 outputs = model.generate(**inputs, max_new_tokens=1024)
 response = processor.decode(outputs[0][input_len:], skip_special_tokens=False)
+perf2 = perf_counter()
+
+print(f"Model now Generates: {perf2-perf}")
 
 # Parse output
 processor.parse_response(response)
