24adea0e73efc7e0835e1fae15b96a4f71180124
Aman Nindra <amannindra@gmail.com>
2026-09-02 18:14:18 -0700

    das

--- main.py
+++ main.py
@@ -44,27 +44,33 @@ def main():
     print("This is the main function.")
     ds_art, ds_unlabel, ds_label = load_data()
     
-    example = ds_art['train'][0]
-    question = example["question"]
-    answer = example["final_decision"]
-    context = example["context"]["contexts"]
-    prompt = make_prompt(question, context)
+    correct = 0
+    incorrect = 0
+    for i in range(len(ds_art['train']) // 1000):
+        example = ds_art['train'][0]
+        
+        
+        question = example["question"]
+        answer = example["final_decision"]
+        context = example["context"]["contexts"]
+        prompt = make_prompt(question, context)
 
-    model = LLM()
-    
-    thinking_content, content = model.inference(prompt)
-    
-    if content == "":
-        print("Output is empty.")
-        print(f"Expected Answer: {answer}, but got: {content}")    
-    elif content == answer:
-        print("Output matches the answer.")
-        print(f"Answer: {answer}, and got: {content}")
-    else:
-        print("Output does not match the answer.")
-        print(f"Expected Answer: {answer}, but got: {content}")    
-
-
+        model = LLM()
+        
+        thinking_content, content = model.inference(prompt)
+        
+        if content == answer:
+            print("Output matches the answer.")
+            print(f"Answer: {answer}, and got: {content}")
+            correct += 1
+        else:
+            print("Output does not match the answer.")
+            print(f"Answer: {answer}, and got: {content}")
+            incorrect += 1
+            
+            
+    print(f"Correct: {correct}, Incorrect: {incorrect}, Total: {correct + incorrect}")
+    print(f"Accuracy: {correct / (correct + incorrect) * 100:.2f}%")
 
     
 if __name__ == "__main__":     
