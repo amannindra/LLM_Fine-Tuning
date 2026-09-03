26a2170564ccb89caa848007f64868f22792d120
Aman Nindra <amannindra@gmail.com>
2026-09-02 18:01:16 -0700

    das

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from datasets import load_dataset
 print("Imports Loaded")
 
 
-def make_prompt(example: dict) -> str:
-    question = example["question"]
-    answer = example["final_decision"]
-    context = example["context"]["contexts"]
+def make_prompt(question, context) -> str:
+
 
     return f"""
         Use the medical context below to answer the question.
@@ -15,7 +13,7 @@ def make_prompt(example: dict) -> str:
         {context}
 
         Question:
-        {example["question"]}
+        {question}
 
         Respond with exactly one of these labels:
         yes
@@ -36,13 +34,27 @@ def load_data():
 def main():
     print("This is the main function.")
     ds_art, ds_unlabel, ds_label = load_data()
-    print(f"Prompt: {make_prompt(ds_art['train'][0])}")
+    
+    example = ds_art['train'][0]
+    question = example["question"]
+    answer = example["final_decision"]
+    context = example["context"]["contexts"]
+    prompt = make_prompt(question, context)
 
     model = LLM()
     
-    thinking_content, content = model.inference(make_prompt(ds_art['train'][0]))
-    print(f"Thinking Content: {thinking_content}")
-    print(f"Content: {content}")  
+    thinking_content, content = model.inference(prompt)
+    
+    if content == "":
+        print("Output is empty.")
+        print(f"Expected Answer: {answer}, but got: {content}")    
+    elif content == answer:
+        print("Output matches the answer.")
+        print(f"Answer: {answer}, and got: {content}")
+    else:
+        print("Output does not match the answer.")
+        print(f"Expected Answer: {answer}, but got: {content}")    
+
 
 
     
